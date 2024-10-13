e75ded83cba73586124cce40c7540578d2ae3d9d
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-10-13 11:35:26 -0400

    imports and routelist start

--- react-router-dogfinder/src/App.js
+++ react-router-dogfinder/src/App.js
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BrowserRouter } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
---
+++ react-router-dogfinder/src/RouteList.js
@@ -0,0 +1,7 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import DogList from './DogList';
+import FilterDogDetails from './FilterDogDetails';
+
+function RouteList({ dogs }) {
+  return <Routes> </Routes>;
+}
